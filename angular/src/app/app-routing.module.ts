b76a1dc5f779406f613480c09882d5b86057ea41
Ajay Kumar <ajayarya101@gmail.com>
2025-05-01 22:25:32 +0530

    adding folder structure

---
+++ angular/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+
+const routes: Routes = [
+  { path: 'products', component:ProductListComponent }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ angular/src/app/services/product.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private apiUrl = 'http://localhost:5000/api/products';
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  addProduct(product: any): Observable<any> {
+    return this.http.post(this.apiUrl, product);
+  }
+}
